e699103295fc64161ec07d77505127c15b9c83d4
Zach Garman <zachgarman@gmail.com>
2016-10-14 18:45:33 -0500

    server and router complete, connect to db correctly

--- routes/todo.js
+++ routes/todo.js
@@ -2,7 +2,7 @@ var router = require('express').Router();
 var pg = require('pg');
 
 var config = {
-  database: 'todoslist'
+  database: 'todolist'
 }
 
 var pool = new pg.Pool(config);
@@ -16,12 +16,40 @@ router.get('/', function (req, res) {
         return;
       }
 
-      client.query('SELECT * FROM owners;', function (err, result) {
+      client.query('SELECT * FROM todos;', function (err, result) {
         if (err) {
           console.log('Error querying DB', err);
           res.sendStatus(500);
           return;
         }
+        console.log(result.rows);
+        res.send(result.rows);
+      });
+    } finally {
+      done();
+    }
+  });
+});
+
+router.post('/', function(req, res) {
+  pool.connect(function(err, client, done) {
+    try {
+      if (err) {
+        console.log('Error connecting to DB', err);
+        res.sendStatus(500);
+        return;
+      }
+
+      client.query('INSERT INTO todos (list_item, crossed_off) ' +
+      'VALUES ($1, $2);',
+      [req.body.listItem, req.body.crossedOff],
+      function(err, result) {
+        if (err) {
+          console.log('Error querying DB', err);
+          res.sendStatus(500);
+          return;
+        }
+        console.log(result.rows);
         res.send(result.rows);
       });
     } finally {
